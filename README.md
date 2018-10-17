bda7fe7e177b4c322bc9ca20e5ffadcf1e4ea2a0
KabanChan <natsumi@chodangm.goe.go.kr>
2018-10-18 00:13:24 +0900

    Implemented all core game routine and templeted main game. Game function may be added in next commit. No eta

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # matchfive
 Just a little game. Very experimental and no eta for graphics. Completely written in Python 3.
+# Licence
+Look at the LICENCE file. Licenced with GNU GPL Version 3
---
+++ maingame.py
@@ -0,0 +1,8 @@
+import gameroutine
+__author__ = "Woohyun Cho (aka KafuChinoDesu in GitHub)"
+
+w, h = 10, 10;
+Board = [[" " for x in range(w)] for y in range(h)]
+
+// TODO : Implement this.
+
