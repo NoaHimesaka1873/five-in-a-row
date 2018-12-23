05db1ad26f68fb854479ca6af0d8c22eb513a186
KabanChan <natsumi@chodangm.goe.go.kr>
2018-12-23 11:35:40 +0900

    UPDATE: 1.0.0

--- gameroutine.py
+++ gameroutine.py
@@ -1,29 +1,33 @@
 #This Module checks if the stones are placed in 5 in the row.
 __author__ = "Woohyun Cho (aka KafuChinoDesu in GitHub)"
 
-function printboard(board, w, h)
+def printboard(board, w, h):
+    xpos = 0
+    ypos = 0
     print(("+-"*w) + "+" )
-    for y in w:
+    for ypos in range(0,w):
         line = ""
-        for x in h:
-            line += "|{0}".format(board[x][y])
+        for xpos in range(0,h):
+            line += "|{0}".format(board[xpos][ypos])
         print(line + "|")
         print(("+-"*w) + "+" )
-    print(("+-"*w) + "+" )
+    
 
 
 
 
-function ifstonesarein5intherow(board, w, h):
-    for x in w-4:
-        for y in h-4:
-            if board[x][y] == board[x+1][y] and board[x+1][y] == board[x+2][y] and board[x+2][y] == board[x+3][y] and board[x+3][y] == board[x+4][y]: // check board horizontally
-                return board[x][y]
-            elif board[x][y] == board[x][y+1] and board[x][y+1] == board[x][y+2] and board[x][y+2] == board[x][y+3] and board[x][y+3] == board[x][y+4]: // check board up to down
-                return board[x][y]
+def ifstonesarein5intherow(board, w, h):
+    tempvar = None
+    for x in range(0,w-4):
+        for y in range(0,h-4):
+            if board[x][y] == board[x+1][y] and board[x+1][y] == board[x+2][y] and board[x+2][y] == board[x+3][y] and board[x+3][y] == board[x+4][y]: 
+                tempvar = board[x][y]
+            elif board[x][y] == board[x][y+1] and board[x][y+1] == board[x][y+2] and board[x][y+2] == board[x][y+3] and board[x][y+3] == board[x][y+4]: 
+                tempvar = board[x][y]
             elif board[x][y] == board[x+1][y+1] and board[x+1][y+1] == board[x+2][y+2] and board[x+2][y+2] == board[x+3][y+3] and board[x+3][y+3] == board[x+4][y+4]:
-                return board[x][y]
+                tempvar = board[x][y]
             elif board[x-4][y] == board[x-3][y+1] and board[x-3][y+1] == board[x-2][y+2] and board[x-2][y+2] == board[x-1][y+3] and board[x-1][y+3] == board[x][y+4]:
-                return board[x][y]
+                tempvar = board[x][y]
             else:
-                return 0;
+                tempvar = 0
+    return tempvar
--- maingame.py
+++ maingame.py
@@ -3,6 +3,23 @@ __author__ = "Woohyun Cho (aka KafuChinoDesu in GitHub)"
 
 w, h = 10, 10;
 Board = [[" " for x in range(w)] for y in range(h)]
+ruwin = None
 
-// TODO : Implement this.
-
+gameroutine.printboard(Board, w, h)
+while True:   
+    input = input("Input(ex: \"1,1\" for 1,1):")
+    inputx, inputy = input.split()
+    Board[inputx][inputy] = "O"
+    ruwin = gameroutine.ifstonesarein5intherow(Board, w, h)
+    if ruwin != None:
+        print(ruwin + " won the game!")
+        break
+    gameroutine.printboard(Board, w, h)
+    input = input("Input(ex: \"1,1\" for 1,1):")
+    inputx, inputy = input.split()
+    Board[inputx][inputy] = "X"
+    ruwin = gameroutine.ifstonesarein5intherow(Board, w, h)
+    gameroutine.printboard(Board, w, h)
+    if ruwin != None:
+        print(ruwin + " won the game!")
+        break
