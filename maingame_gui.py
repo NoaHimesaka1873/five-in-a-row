5755adeaa1a381b23c2964d9c89f00317cf67aed
KafuChinoDesu <natsumi@chodangm.goe.go.kr>
2020-08-18 00:36:11 +0900

    Added comments to gui version of the game to make this code easier to understand.

--- maingame_gui.py
+++ maingame_gui.py
@@ -19,16 +19,23 @@ __license__ = f"""
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-w, h = 10, 10
-Board = [[0 for x in range(w)] for y in range(h)]
+w, h = 10, 10  # Sets the width and height of the board
+Board = [[0 for x in range(w)] for y in range(h)]  # Initializes the board
 
+# This starts PyGame engine.
 pygame.init()
 
-size = width, height = 510, 510
-black = 0, 0, 0
-white = 255, 255, 255
-rose_gold = 183, 110, 121
-gold = 212, 175, 55
+# DO NOT EDIT THESE VALUES UNLESS IF YOU KNOW WHAT YOU'RE DOING
+size = width, height = 510, 510  # Sets the size of the window
+# You can edit these colours to whatever you want. Just be careful.
+black = 0, 0, 0  # Colour Black
+white = 255, 255, 255  # Colour White
+rose_gold = 183, 110, 121  # Colour Rose Gold (Player 1 Colour)
+gold = 212, 175, 55  # Colour Gold (Player 2 Colour)
+
+# Sets player colours
+p1colour = rose_gold
+p2colour = gold
 
 # This sets the WIDTH and HEIGHT of each grid location
 WIDTH = 40
@@ -37,8 +44,8 @@ HEIGHT = 40
 # This sets the margin between each cell
 MARGIN = 10
 
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("")
+screen = pygame.display.set_mode(size)  # Sets screen size according to size variable
+pygame.display.set_caption("Five-in-a-row")  # Sets screen title
 
 # Loop until the user clicks the close button.
 done = False
@@ -56,10 +63,11 @@ while not done:
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
         elif event.type == pygame.MOUSEBUTTONDOWN:
+            # If no one won the game
             if winner is None:
-                now = pygame.time.get_ticks()
+                now = pygame.time.get_ticks()  # Gets the current game tick
                 if now - last >= cooldown:
-                    last = now
+                    last = now  # Compares it with the cooldown to prevent misclick
                     # User clicks the mouse. Get the position
                     pos = pygame.mouse.get_pos()
                     # Change the x/y screen coordinates to grid coordinates
@@ -68,11 +76,14 @@ while not done:
                     # Set that location to one
                     if Board[row][column] == 0:
                         Board[row][column] = turn
+                    # Swaps the turn
                     if turn == 1:
                         turn = 2
                     elif turn == 2:
                         turn = 1
+                    # Log position and grid coordinates
                     print("Click ", pos, "Grid coordinates: ", row, column)
+                    # Check if someone won the game
                     winner = gameroutine.checkstone(Board, w, h, 0)
 
     # Set the screen background
@@ -81,13 +92,13 @@ while not done:
     # Draw the grid
     for row in range(10):
         for column in range(10):
-            color = white
+            colour = white
             if Board[row][column] == 1:
-                color = rose_gold
+                colour = p1colour
             elif Board[row][column] == 2:
-                color = gold
+                colour = p2colour
             pygame.draw.rect(screen,
-                             color,
+                             colour,
                              [(MARGIN + WIDTH) * column + MARGIN,
                               (MARGIN + HEIGHT) * row + MARGIN,
                               WIDTH,
@@ -96,14 +107,18 @@ while not done:
     # Limit to 60 frames per second
     clock.tick(60)
 
+    # If there is a winner, fill the screen with solid black colour and say about who won the game
     if winner is not None:
+        # Log the winner to the console
         print("Winner : ", winner)
-        screen.fill(black)
-        font = pygame.font.Font(None, 30)
-        text = font.render("Player {} won the game!".format(winner), 1, gold)
+        screen.fill(black)  # Fill the screen with black
+        font = pygame.font.Font(None, 30)  # Set the font to whatever the system tells us to use
+        text = font.render("Player {} won the game!".format(winner), 1, gold)  # Set the message
+        # Set the position to the center of the screen
         textpos = text.get_rect()
         textpos.centerx = screen.get_rect().centerx
         textpos.centery = screen.get_rect().centery
+        # Make the text visible
         screen.blit(text, textpos)
 
     # Go ahead and update the screen with what we've drawn.
